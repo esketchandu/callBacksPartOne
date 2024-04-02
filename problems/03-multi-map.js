14b66a0760f134c45abd82bced46909bb73a03d5
esketchandu <esketchandu10@gmail.com>
2024-04-01 22:01:43 -0700

    03 solution

--- problems/03-multi-map.js
+++ problems/03-multi-map.js
@@ -7,23 +7,50 @@ Examples:
 
 let result1 = multiMap(7, 2, function(n) {
   return n * 10;
+  // 7 * 10 = 70
+  // 70 * 10 = 700
 });
 console.log(result1); // 700
 
 let result2 = multiMap(7, 3, function(n) {
     return n * 10;
+    // 7 * 10 = 70
+    // 70 * 10 = 700
+    //700 * 10 = 7000
 });
 console.log(result2); // 7000
 
 let result3 = multiMap("hi", 5, function(s) {
   return s + "!";
+  // hi!
 });
 console.log(result3); // hi!!!!!
 *******************************************************************************/
 
 function multiMap(val, n, cb) {
-  // Your code here 
+  let temp = cb(val)
+  for (let i = 0; i < n-1; i++) {
+      temp = cb(temp)
+    }
+ return temp
 }
+
+
+let result1 = multiMap(7, 2, function(n) {
+  return n * 10;
+});
+console.log(result1); // 700
+
+let result2 = multiMap(7, 3, function(n) {
+  return n * 10;
+});
+console.log(result2); // 7000
+
+let result3 = multiMap("hi", 5, function(s) {
+return s + "!";
+// hi!
+});
+console.log(result3); // hi!!!!!
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
