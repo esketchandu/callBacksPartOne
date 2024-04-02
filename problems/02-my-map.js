cf9dd4c8d9e35f2534a6e79d6bee511c3dd21309
grassella <negrete.graciela@yahoo.com>
2024-04-01 23:31:52 -0500

    02 running, not passed

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -17,9 +17,22 @@ console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 *******************************************************************************/
 
 function myMap(array, cb) {
-    // Your code here 
+     const resultArr = [];
+
+     for (let el of array) {
+        const newEl = cb(el);
+        resultArr.push(newEl)
+     }
+     return resultArr;
 }
 
+let result1 = myMap([100, 25, 81, 64], Math.sqrt);
+console.log(result1);   // [ 10, 5, 9, 8 ]
+
+let result2 = myMap(['run', 'Forrest'], function (el) {
+    return el.toUpperCase() + '!';
+});
+console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
     module.exports = myMap;
