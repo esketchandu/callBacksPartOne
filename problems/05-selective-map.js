d9a0adf1743b79d8f5a1302f3e42254cb5099c54
grassella <negrete.graciela@yahoo.com>
2024-04-02 21:30:59 -0500

    05 solved

--- problems/05-selective-map.js
+++ problems/05-selective-map.js
@@ -37,8 +37,44 @@ console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isPositive, square));
 *******************************************************************************/
 
 function selectiveMap(array, selector, mapper) {
-    // Your code here 
+    let newArr = [];
+    for (let i = 0; i < array.length; i++) {
+        let temp = selector(array[i])
+        if (temp) {
+            let temp2 = mapper(array[i])
+            newArr.push(temp2)
+        }
+        else {
+            newArr.push(array[i])
+        }
+    }
+    return newArr;
 }
+
+function isEven(n) {
+    return n % 2 === 0;
+}
+
+function isPositive(n) {
+    return n > 0;
+}
+
+function square(n) {
+    return n * n;
+}
+
+function flipSign(n) {
+    return n * -1;
+}
+
+console.log(selectiveMap([8, 5, 10, 4], isEven, square));
+// [ 64, 5, 100, 16 ]
+
+console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isEven, flipSign));
+// [ 10, -4, 7, -6, 2, -9 ]
+
+console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isPositive, square));
+// [-10, 16, 49, 36, -2, -9]
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
