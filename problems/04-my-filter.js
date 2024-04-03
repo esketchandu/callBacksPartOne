35bc4db32171a80a5ea880183a0fb23c05e741c9
esketchandu <esketchandu10@gmail.com>
2024-04-02 19:17:46 -0700

    04-solution

--- problems/04-my-filter.js
+++ problems/04-my-filter.js
@@ -20,8 +20,24 @@ console.log(result2);      // ['choose', 'words', 'only']
 *******************************************************************************/
 
 function myFilter(array, cb) {
-    // Your code here 
+    let newarr = [];
+    for (let i = 0; i < array.length; i++) {
+        let temp = cb(array[i])
+         if (temp) {
+        newarr.push(array[i]) }
+    }
+ return newarr
 }
+
+let result1 = myFilter([5, 7, 4, 3, 8], function (n) {
+    return n % 2 === 0;
+});
+console.log(result1);       // [ 4, 8 ]
+
+let result2 = myFilter(['choose', 'big', 'words', 'only'], function (s) {
+    return s.length > 3;
+});
+console.log(result2);      // ['choose', 'words', 'only']
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
